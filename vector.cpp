d54bcf286fbf00ebcc0902979639829d74781377
longvien <vienngoclongvn@gmail.com>
2026-04-24 23:13:37 +0200

    vector

---
+++ vector.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    /*vector<int> v;
+    for (int i = 0; i < 5; i ++) {
+        v.push_back(i);
+    }
+    for (int num : v) {
+        cout << num << " ";
+    };
+    return 0; 
+
+    pair<string, int> n;
+    n.first = "A";
+    int a = 90;
+    n.second = '1';
+    cout << n.first << " " << n.second;
+    return 0; */
+
+
+
+    int x = 5;
+    int y = 10;
+    int z = x + y;   // breakpoint here
+    cout << z << endl;
+
+}
