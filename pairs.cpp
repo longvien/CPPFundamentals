6ff42fae8e94df8ba391646ef96facc205268cc3
longvien <vienngoclongvn@gmail.com>
2026-04-25 19:32:53 +0200

    second implementation option for pair

--- pairs.cpp
+++ pairs.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 int main() {
+    // First initialize option
     pair<string, int> n;
     n.first = "A";
     n.second = 1;
-    cout << n.first << " - " << n.second;
+    cout << n.first << " - " << n.second << endl;
+    // Second initialize option
+    pair<double, char> p = {2.5, 'a'};
+    cout << p.first << " " << p.second;
     return 0; 
+    // Notice: Pair is a cointainer of ONLY 2 elements!
 }
