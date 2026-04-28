fdda667ebb462d53d8856b27fb7af521140c4430
longvien <vienngoclongvn@gmail.com>
2026-04-28 22:06:00 +0200

    automatic calculator

--- calculator.cpp
+++ calculator.cpp
@@ -20,10 +20,28 @@ double divide(double a, double b) {
             return a / b;
         }
         else {
-            throw ("Zero Division Error");}
-    } catch(string err) {
-        cout << err;
+            throw (string("Zero Division Error"));
+        }
+    } 
+    catch(string err) {
+        cout << err << " ";
     }
-    
+    return 0.0;
 }
-int main() {}
+
+int main() {
+    double firstNum = 2.0;
+    double secondNum = 3.0;
+    string symbol = "+";
+    // cout << "Pls enter the first number" << endl;
+    // cin >> firstNum;
+    // cout << "Pls enter the second number" << endl;
+    // cin >> secondNum;
+    // cout << "Pls enter the calulation symbol" << endl;
+    // cin >> symbol;
+
+    if (symbol == string("+")) { cout << (firstNum, secondNum); }
+    else if (string(symbol) == "-") { cout << subtract(firstNum, secondNum); }  
+    else if (string(symbol) == "*") { cout << multiply(firstNum, secondNum); }
+    else if (string(symbol) == "/") { cout << divide(firstNum, secondNum); } 
+}
