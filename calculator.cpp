a4b6be9cd7c8c3e6e36fd6cbfda4efb08e328a8f
longvien <vienngoclongvn@gmail.com>
2026-04-29 23:08:00 +0200

    automatic calculator

--- calculator.cpp
+++ calculator.cpp
@@ -1,47 +1,47 @@
-#include <bits/stdc++.h>
+    #include <bits/stdc++.h>
 
-using namespace std;
+    using namespace std;
 
-double add(double a , double b) {
-    return a + b;
-}
-
-double subtract(double a, double b) {
-    return a - b;
-}
-
-double multiply(double a, double b) {
-    return a * b;
-}
-
-double divide(double a, double b) {
-    try {
-        if (b != 0) {
-            return a / b;
-        }
-        else {
-            throw (string("Zero Division Error"));
-        }
-    } 
-    catch(string err) {
-        cout << err << " ";
+    double add(double a , double b) {
+        return a + b;
     }
-    return 0.0;
-}
 
-int main() {
-    double firstNum = 2.0;
-    double secondNum = 3.0;
-    string symbol = "+";
-    // cout << "Pls enter the first number" << endl;
-    // cin >> firstNum;
-    // cout << "Pls enter the second number" << endl;
-    // cin >> secondNum;
-    // cout << "Pls enter the calulation symbol" << endl;
-    // cin >> symbol;
+    double subtract(double a, double b) {
+        return a - b;
+    }
 
-    if (symbol == string("+")) { cout << (firstNum, secondNum); }
-    else if (string(symbol) == "-") { cout << subtract(firstNum, secondNum); }  
-    else if (string(symbol) == "*") { cout << multiply(firstNum, secondNum); }
-    else if (string(symbol) == "/") { cout << divide(firstNum, secondNum); } 
-}
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        try {
+            if (b != 0) {
+                return a / b;
+            }
+            else {
+                throw (string("Zero Division Error"));
+            }
+        } 
+        catch(string err) {
+            cout << err << " ";
+        }
+        return 0.0;
+    }
+
+    int main() {
+        double firstNum = 2.0;
+        double secondNum = 3.0;
+        string symbol = "+";
+        cout << "Pls enter the first number" << endl;
+        cin >> firstNum;
+        cout << "Pls enter the second number" << endl;
+        cin >> secondNum;
+        cout << "Pls enter the calulation symbol" << endl;
+        cin >> symbol;
+
+        if (symbol == string("+")) { cout << add(firstNum, secondNum); }
+        else if (string(symbol) == "-") { cout << subtract(firstNum, secondNum); }  
+        else if (string(symbol) == "*") { cout << multiply(firstNum, secondNum); }
+        else if (string(symbol) == "/") { cout << divide(firstNum, secondNum); } 
+    }
